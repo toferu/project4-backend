4b51b5d0c65d61b5ce6d5301c8ff9fa350c01db3
Christopher Anderson <tophermichaelson@gmail.com>
2023-01-03 18:34:04 -0500

    fixed typos

--- project4_rest_api/project4_rest_api/urls.py
+++ project4_rest_api/project4_rest_api/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import include # add this
 
 urlpatterns = [
-    path('', include('contacts_api.urls')), # add this
+    path('', include('slammers_api.urls')), # add this
     path('admin/', admin.site.urls),
 ]
