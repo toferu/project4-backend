4080fdb27548436515ebba3986db95456c8a75e9
Christopher Anderson <tophermichaelson@gmail.com>
2023-01-05 09:19:13 -0500

    switched settings.py back to local settings

--- project4_rest_api/project4_rest_api/settings.py
+++ project4_rest_api/project4_rest_api/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-6g$gv)gc))(6_&sxdw31kpw*q*jxaavj0$9t!w)g^m80+mu*g_'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'gunnicornskateboards.herokuapp.com']
 
